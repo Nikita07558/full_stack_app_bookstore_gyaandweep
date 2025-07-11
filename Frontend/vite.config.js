9f432963420fc05afaffa881577b6eb41b1965d3
Nikita07558 <116504279+Nikita07558@users.noreply.github.com>
2025-07-11 17:30:52 +0530

    Update vite.config.js so that render just doesnt see index.html

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ensures routing works correctly in production
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
-})
-
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  base: '/',
+  server: {
+    port: 5173,
+  },
+  // ⬇️ this is the fix
+  // Serve index.html for all routes
+  // So React Router can handle them
+  optimizeDeps: {},
+});
