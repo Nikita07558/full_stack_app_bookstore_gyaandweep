3db5512e9c07b038253f4f1f9fc4d93ff46c5d28
Nikita07558 <116504279+Nikita07558@users.noreply.github.com>
2025-07-11 15:44:25 +0530

    Update Freebook.jsx to deploy chnaged localhost

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -12,7 +12,7 @@ function Freebook() {
   useEffect(()=>{
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/book`);
         console.log(res.data);
         const data=res.data.filter((data) => data.category === "free" );
         setBook(data);
